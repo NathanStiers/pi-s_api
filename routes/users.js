e5063bc7203a8750096f6c5e26ca1b3ae6758c4f
Stiers <nathan.stiers@gmail.oom>
2020-01-01 20:11:28 +0100

    first try

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,7 @@ const db = require('../modules/db')
 // Return All Users
 router.get('/listeUser', function (req, res) {
     db.db.collection('Users').find().toArray().then(all_users => {
+        all_users.forEach(el => delete el.password)      
         res.json(all_users)
     }).catch(err =>{
         console.error("Error with route listeUser GET: " + err.message)
@@ -23,15 +24,17 @@ router.get('/listeUser', function (req, res) {
 // Update hours
 router.post('/hours', function (req, res, next) {
     console.log(req.body)
-    db.db.collection('Users').findOneAndUpdate({name:req.body.name}, {$set: req.body}, {returnOriginal:false})
+    db.db.collection('Users').findOneAndUpdate({name:req.body.name}, {$set: {hours:req.body.hours}}, {returnOriginal:false})
     .then(result => {
         if(result.value){
+            console.log(result.value)
             delete result.value.password
             res.json(result.value)
         }else{
             res.status(404).send()
         }
     }).catch(err => {
+        console.log(err.message)
         res.status(500).send(err)
     })
 })
